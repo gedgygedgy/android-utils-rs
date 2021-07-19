803dc4be2a1c984f01e274ea89caa022b1e08f63
Gedgy Gedgy <gedgygedgy@protonmail.com>
2021-07-19 02:25:18 -0400

    Formatting

--- test/java/src/test/java/io/github/gedgygedgy/rust/android/LogTest.java
+++ test/java/src/test/java/io/github/gedgygedgy/rust/android/LogTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.robolectric.RobolectricTestRunner;
+
 @RunWith(RobolectricTestRunner.class)
 public class LogTest {
     static {
