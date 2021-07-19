55a75b0f22bbb42425805c88a52845efb4293f24
Gedgy Gedgy <gedgygedgy@protonmail.com>
2021-07-19 12:58:59 -0400

    Remove logging functionality
    Programs should instead use the android_log crate. It is superior
    because it uses liblog directly instead of going through the JVM,
    eliminating lots of overhead and JNI-related errors.

--- android-utils/rust/lib.rs
+++ android-utils/rust/lib.rs
@@ -1,16 +1,14 @@
 use jni::{errors::Result, JNIEnv};
 
-pub mod log;
 pub mod os;
 
-/// Initialize [`android-utils`](crate). This initializes the Android logger
-/// implementation. This should be called before using
-/// [`android-utils`](crate).
+/// Initialize [`android-utils`](crate). This currently does nothing, but it
+/// may initialize some JNI functions in the future. This should be called
+/// before using [`android-utils`](crate).
 ///
 /// # Arguments
 ///
 /// * `env` - Java environment to use.
-pub fn init<'a: 'b, 'b>(env: &'b JNIEnv<'a>) -> Result<()> {
-    log::init(env)?;
+pub fn init<'a: 'b, 'b>(_env: &'b JNIEnv<'a>) -> Result<()> {
     Ok(())
 }
